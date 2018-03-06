4c6be68217a33de3d344ec511516e85ca091ee3d
j <julianymark@gmail.com>
2018-03-06 23:51:56 +0100

    code++

--- app.ts
+++ app.ts
@@ -1,14 +1,25 @@
-import $ from 'jquery';
 import * as three from 'three';
 
 document.addEventListener('DOMContentLoaded', start);
 
-const canvas: HTMLCanvasElement = $('#thecanvas')[0] as HTMLCanvasElement;
+const canvas = document.getElementById('thecanvas') as HTMLCanvasElement;
+
+const scene = new three.Scene();
 const renderer = new three.WebGLRenderer({ canvas, antialias: true });
 renderer.setClearColor(0x00ff00);
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
+const width = 20;
+const height = 20;
+const camera = new three.OrthographicCamera(width / - 2, width / 2, height / 2, height / - 2, 0.1, 1000);
+scene.add(camera);
+
+const box = new three.BoxGeometry(10, 10, 10);
+
+renderer.render(scene, camera);
+
 function start() {
     console.log('hi');
+
 }
